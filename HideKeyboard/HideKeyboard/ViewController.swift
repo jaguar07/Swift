a1fc1492ee5afc3dd4dd729f3d26d399798ab933
Carlos Butron <carlosbutron@icloud.com>
2014-12-08 20:53:33 +0100

    Keyboard show/hide

---
+++ HideKeyboard/HideKeyboard/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  HideKeyboard
+//
+//  Created by Carlos Butron on 06/12/14.
+//  Copyright (c) 2014 Carlos Butron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textField1.delegate = self
+        textField2.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    //called when users tap out of textfield
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+
+
+}
+
