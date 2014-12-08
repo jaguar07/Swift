73f893450ea9d394de7fe1cb7444ed66c19dc2b4
Carlos Butron <carlosbutron@icloud.com>
2014-12-08 20:54:57 +0100

    XIB and Storyboards

---
+++ XIBwithStoryboard/SegueWithpresentViewController/NuevaClase.swift
@@ -0,0 +1,35 @@
+//
+//  NuevaClase.swift
+//  T3E3CarlosButron
+//
+//  Created by Carlos Butron on 31/10/14.
+//  Copyright (c) 2014 Carlos Butron. All rights reserved.
+//
+
+import UIKit
+
+class NuevaClase: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
